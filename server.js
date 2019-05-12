ffef2c294df840b8151662e6e35683db9dc4ab7d
S. J. Zhang <sj@s-j-zhang.com>
2019-05-11 22:25:35 -0400

    heroku-pg

--- server.js
+++ server.js
@@ -2,26 +2,30 @@ var express = require('express');
 
 var app = express()
 
-var text = "INSERT INTO users(email) VALUES($1)"
+var text = "INSERT INTO users (email) VALUES ($1);"
 
 app.use(express.static('public'))
 
 var mustacheExpress = require('mustache-express');
 
-//database
+database
 
-// var {
-//   Client
-// } = require('pg');
-// var connectionString = process.env.DATABASE_URL;
+var {
+  Client
+} = require('pg');
+var connectionString = process.env.DATABASE_URL;
 
-// var client = new Client({
-//     connectionString: connectionString,
-//     ssl: true,
-// });
-// var bodyParser = require('body-parser');
+var client = new Client({
+    connectionString: connectionString,
+    ssl: true,
+});
+var bodyParser = require('body-parser');
 
-// Register '.mustache' extension with The Mustache Express
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
 app.engine('html', mustacheExpress());
 
 app.set('view engine', 'html');
@@ -31,18 +35,18 @@ app.get('/', function(req,res){
 })
 
 app.get('/', function(req,res){
-    // res.send("hello")
+    res.send("hello")
 })
 
-// app.post('/post', function(req, res) {
-//   console.log(req.body.email)
-//   input = [req.body.comment]
+app.post('/post', function(req, res) {
+  console.log(req.body.email)
+  input = [req.body.comment]
 
-//   client.query(text, input, (err, res) => {
-//     if (err) throw err;
-//   })
-//   res.redirect('/')
-// })
+  client.query(text, input, (err, res) => {
+    if (err) throw err;
+  })
+  res.redirect('/')
+})
 
 app.listen(8000, function(){
   console.log("connected")
