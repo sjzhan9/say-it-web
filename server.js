54ef21956351e28d6178200893c48175163333ef
S. J. Zhang <sj@s-j-zhang.com>
2019-05-11 21:37:50 -0400

    first-commit-on-heroku

--- server.js
+++ server.js
@@ -2,9 +2,24 @@ var express = require('express');
 
 var app = express()
 
+var text = "INSERT INTO users(email) VALUES($1)"
+
 app.use(express.static('public'))
 
 var mustacheExpress = require('mustache-express');
+
+//database
+
+var {
+  Client
+} = require('pg');
+var connectionString = process.env.DATABASE_URL;
+
+var client = new Client({
+    connectionString: connectionString,
+    ssl: true,
+});
+var bodyParser = require('body-parser');
 
 // Register '.mustache' extension with The Mustache Express
 app.engine('html', mustacheExpress());
@@ -16,7 +31,17 @@ app.get('/', function(req,res){
 })
 
 app.get('/', function(req,res){
-    res.send("hello")
+    // res.send("hello")
+})
+
+app.post('/post', function(req, res) {
+  console.log(req.body.email)
+  input = [req.body.comment]
+
+  client.query(text, input, (err, res) => {
+    if (err) throw err;
+  })
+  res.redirect('/')
 })
 
 app.listen(8000, function(){
